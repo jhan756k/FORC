f4fb96d266e1f6ad2af3f61d1b26ce4ebcd07127
Jooney <42194130+jhan756k@users.noreply.github.com>
2023-03-29 15:29:54 +0900

    member init

--- src/Page/Aboutpage.jsx
+++ src/Page/Aboutpage.jsx
@@ -1,43 +1,67 @@
-import React, {useEffect} from 'react';
-import Sticknav from '../Component/Sticknav';
+import React, { useEffect, useState } from "react";
+import Sticknav from "../Component/Sticknav";
 import "../Style/Aboutpage.css";
 
 const Aboutpage = () => {
+  const handleScroll = () => {
+    document.querySelector(".stick").style.backgroundColor =
+      "rgba(255,255,255,1)";
+  };
 
-    const handleScroll = () => {
-        document.querySelector('.stick').style.backgroundColor = "rgba(255,255,255,1)";
+  useEffect(() => {
+    document.querySelector(".stick").style.backgroundColor =
+      "rgba(255,255,255,1)";
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
     };
+  }, []);
 
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll, { passive: true });
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []); 
+  const [member, setMember] = useState([
+    {
+      img: "/images/1.jpg",
+      name: "서우찬",
+      position: "대표",
+      explain: "자연 생태계 보존을 위한 연구를 진행하는 FORC 입니다.",
+    },
+    {
+      img: "/images/2.jpg",
+      name: "김태훈",
+      position: "대표",
+      explain: "자연 생태계 보존을 위한 연구를 진행하는 FORC 입니다.",
+    },
+    {
+      img: "/images/3.jpg",
+      name: "김태훈",
+      position: "대표",
+      explain: "자연 생태계 보존을 위한 연구를 진행하는 FORC 입니다.",
+    },
+  ]);
 
-    return (
-        <div>
-            <Sticknav/>
-            <img src="/images/about.jpg" alt="logo" className="aboutimg" />
-            <div className="abouttext">
-                <h1>ABOUT US</h1>
-                <p>자연 생태계 보존을 위한 &nbsp;연구를 진행하는 FORC 입니다.</p>
-            </div>
-
-            <div className="fields">
-                <div className="forestfield">
-                    
-                </div>
-                <div className="beekeepingfield">
-
-                </div>
-                <div className="pollutionfield">
-
-                </div>
-            </div>
-
+  return (
+    <div className="aboutwrap">
+      <div className="awr">
+        <Sticknav />
+        <div className="oim">
+          <img src="/images/about.jpg" alt="logo" className="aboutimg" />
+          <div className="abouttext">
+            <h1>ABOUT US</h1>
+            <p>자연 생태계 보존을 위한 &nbsp;연구를 진행하는 FORC 입니다.</p>
+          </div>
         </div>
-    );
+      </div>
+
+      <div className="fields">
+        <div className="president">
+          <h1 className="position">대표이사</h1> 
+          
+        </div>
+        <div className="forestfield"></div>
+        <div className="beekeepingfield"></div>
+        <div className="pollutionfield"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Aboutpage;
